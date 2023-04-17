8e60dfebf018b63bb864eabe16f55d055e1cd206
Ruth Wilson <c.r.wilson556@gmail.com>
2023-04-17 02:54:55 -0400

    state machine add

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;  // movement speed of the player
+    public Transform cam;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");  // get input from horizontal axis (left/right arrow keys)
+        float verticalInput = Input.GetAxis("Vertical");  // get input from vertical axis (up/down arrow keys)
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);  // create a movement vector based on input
+        transform.position += movement * moveSpeed * Time.deltaTime;  // move the player based on the movement vector and speed
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.SimpleMove(moveDir.normalized * moveSpeed * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -4,31 +4,46 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    CharacterController controller;
-    public float speed = 5;
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+
+    public CharacterController controller;
     public Transform cam;
 
-    private void Start()
-    {
-        controller = GetComponent<CharacterController>();
-    }
-    // Update is called once per frame
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.SimpleMove(moveDir.normalized  * speed * Time.deltaTime);
+            controller.SimpleMove(moveDir.normalized * speed);
+
+            // Apply gravity
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
         }
     }
 }
